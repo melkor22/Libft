bb1c96c3c251aa7428befe16b1bfdbbb226209b5
Yanis <melkor22@outlook.fr>
2017-07-18 21:12:10 +0200

    memset fait

--- Libft.c
+++ Libft.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
 
+void *ft_memset(void *s,int c,size_t n)
+{
+  char *ps = s;
+  size_t compteur = 0;
+  while(compteur != n)
+  {
+    ps[compteur] = (char)c;
+    compteur++;
+  }
+  return s;
+}
